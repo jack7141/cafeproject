b8527ee0ce3919bce00ebe5b3fb0369ffe185784
황광회 <ghl92479@gmail.com>
2022-03-22 03:44:02 +0900

    setting

--- config/settings.py
+++ config/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -132,11 +131,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = '/static/'
 
 # TODO:static폴더에 있는 요소를 사용하기 위해선 아래항목을 추가해줘야함!!!
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 # Default primary key field type
